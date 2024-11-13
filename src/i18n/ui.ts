3965b537b5b1f092b6d412e2c01270e428c9de18
Pedro Plasencia <programacion.en.esp@gmail.com>
2024-11-13 20:27:20 +0100

    feat: initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languageList = {
+    es: "Español",
+    en: "English"
+}
+
+export const labels = {
+    es: {
+        "nav.home": "Inicio",
+        "nav.about": "Acerca de",
+        "home.title": "Bienvenidos a Programación en español",
+        "about.title": "Acerca de la comunidad",
+    },
+    en: {
+        "nav.home": "Home",
+        "nav.about": "About",
+        "home.title": "Welcome to Programación en español",
+        "about.title": "About our community",
+    },
+};
